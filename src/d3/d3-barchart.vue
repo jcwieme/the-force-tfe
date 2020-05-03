1e8409f0001f8983061fba82ef6998dfaafc09b4
jcwieme <jc.wieme@outlook.com>
2020-05-03 17:56:27 +0200

    :+1: Working on Numbers

---
+++ src/d3/d3-barchart.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bar">Coucou</div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+import * as d3 from 'd3'
+
+export default defineComponent({
+  name: 'd3Bar',
+  setup() {
+    console.log(d3)
+  },
+})
+</script>
+
+<style lang="scss"></style>
--- src/views/Numbers.vue
+++ src/views/Numbers.vue
@@ -1,13 +1,62 @@
 <template>
   <div class="numbers">
     <div>Numbers</div>
+    <d3-bar />
   </div>
 </template>
 
 <script>
-export default {
+import { defineComponent, computed } from '@vue/composition-api'
+
+import d3Bar from '@/d3/d3-barchart'
+
+export default defineComponent({
   name: 'Numbers',
-}
+  components: {
+    d3Bar,
+  },
+  setup(props, ctx) {
+    const numbers = computed(() => {
+      return ctx.root.$store.state.numbers[ctx.root.$store.state.activeMovie]
+    })
+
+    const linesInMovies = computed(() => {
+      return ctx.root.$store.state.linesInMovie.reduce((t, n) => t + n)
+    })
+
+    const wordsInMovies = computed(() => {
+      return ctx.root.$store.state.wordsInMovie.reduce((t, n) => t + n)
+    })
+
+    const linesInMovie = computed(() => {
+      return ctx.root.$store.state.linesInMovie[
+        ctx.root.$store.state.activeMovie
+      ]
+    })
+
+    const wordsInMovie = computed(() => {
+      return ctx.root.$store.state.wordsInMovie[
+        ctx.root.$store.state.activeMovie
+      ]
+    })
+
+    console.log(
+      wordsInMovies.value,
+      linesInMovies.value,
+      wordsInMovie.value,
+      linesInMovie.value,
+      numbers.value
+    )
+
+    return {
+      numbers,
+      linesInMovies,
+      wordsInMovies,
+      linesInMovie,
+      wordsInMovie,
+    }
+  },
+})
 </script>
 
 <style lang="scss">
