a6bc22bf3f0425248211b0fb2d3ec8c21262b2c7
jcwieme <jc.wieme@outlook.com>
2020-05-06 16:00:26 +0200

    :dvd: Donut chart add

---
+++ src/d3/d3-piechart.vue
@@ -0,0 +1,157 @@
+<template>
+  <div class="numbers__column" :class="dataChart.class">
+    <h3 class="numbers__title">{{ dataChart.title }}</h3>
+    <div class="numbers__graph" :id="dataChart.id"></div>
+    <div class="numbers__info">
+      <p class="numbers__total">
+        {{ dataChart.value }}<span class="numbers__special">%</span>
+      </p>
+      <p>{{ dataChart.sub }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent, onMounted } from '@vue/composition-api'
+import * as d3 from 'd3'
+
+export default defineComponent({
+  name: 'd3Pie',
+  props: {
+    dataChart: {
+      type: Object,
+    },
+  },
+  setup(props) {
+    onMounted(() => {
+      var width = props.dataChart.size
+      var height = props.dataChart.size
+      var margin = 10
+      var data = props.dataChart.data
+        .slice()
+        .sort((a, b) => d3.ascending(a.value, b.value))
+
+      var radius = Math.min(width, height) / 2 - margin
+
+      var svg = d3
+        .select(`#${props.dataChart.id}`)
+        .append('svg')
+        .attr('width', width + margin)
+        .attr('height', height)
+        .append('g')
+        .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
+
+      var gDefs = svg.append('g').attr('id', 'gDefs')
+      //Filter for the outside glow
+      var filter = gDefs.append('filter').attr('id', 'glow')
+      filter
+        .append('feGaussianBlur')
+        .attr('stdDeviation', '2')
+        .attr('result', 'coloredBlur')
+      var feMerge = filter.append('feMerge')
+      feMerge.append('feMergeNode').attr('in', 'coloredBlur')
+      feMerge.append('feMergeNode').attr('in', 'SourceGraphic')
+
+      var pie = d3
+        .pie()
+        .sort(null)
+        .value(function(d) {
+          return d.value.value
+        })
+
+      var arc = d3
+        .arc()
+        .innerRadius(Math.min(width, height) / 3.2)
+        .outerRadius(radius)
+        .padAngle(0.05)
+
+      var data_ready = pie(d3.entries(data))
+      var circles = svg
+        .selectAll('portions')
+        .data(data_ready)
+        .enter()
+        .append('path')
+        .attr('id', d => {
+          return 'circle_' + d.data.value.name
+        })
+        .attr('class', 'portions')
+        .attr('d', arc)
+        .attr('fill', 'transparent')
+        .attr('stroke', function(d) {
+          return d.data.value.color
+        })
+        .style('stroke-width', '3')
+        .style('stroke-alignement', 'inner')
+        .style('opacity', 0.7)
+        .attr('filter', 'url(#glow)')
+
+      svg
+        .append('text')
+        .attr('id', `text_${props.dataChart.id}`)
+        .attr('text-anchor', 'middle')
+        .attr('font-size', '14px')
+        .attr('fill', 'white')
+        .attr('opacity', 0)
+        .attr('y', 20)
+        .text('')
+
+      svg
+        .append('text')
+        .attr('id', `number_${props.dataChart.id}`)
+        .attr('text-anchor', 'middle')
+        .attr('font-size', '28px')
+        .attr('fill', 'white')
+        .attr('opacity', 0)
+        .attr('y', -5)
+        .text('')
+
+      circles.on('mouseover', d => {
+        // console.log(d)
+
+        d3.select(`#text_${props.dataChart.id}`)
+          .text(d.data.value.name)
+          .transition()
+          .attr('opacity', 1)
+
+        d3.select(`#number_${props.dataChart.id}`)
+          .text(d.data.value.value)
+          .transition()
+          .attr('opacity', 1)
+
+        d3.select(`#circle_${d.data.value.name}`)
+          .transition()
+          .attr('fill', d.data.value.color)
+      })
+
+      circles.on('mouseout', d => {
+        d3.select(`#text_${props.dataChart.id}`)
+          .transition()
+          .attr('opacity', 0)
+
+        d3.select(`#number_${props.dataChart.id}`)
+          .transition()
+          .attr('opacity', 0)
+
+        d3.select(`#circle_${d.data.value.name}`)
+          .transition()
+          .attr('fill', 'transparent')
+      })
+    })
+  },
+})
+</script>
+
+<style lang="scss">
+.bar {
+  fill: transparent;
+  cursor: pointer;
+}
+.tick {
+  font-family: star_jediregular;
+  letter-spacing: 0.1em;
+}
+
+.portions {
+  cursor: pointer;
+}
+</style>
--- src/views/Numbers.vue
+++ src/views/Numbers.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="numbers">
-    <div class="numbers__bars">
+    <div class="numbers__row">
       <d3-bar :data-bar="barChartDataWords" />
+      <!-- <d3-pie :data-chart="racesData" /> -->
       <d3-bar :data-bar="barChartData" />
     </div>
+    <!-- <div class="numbers__row">
+      <div class="numbers__column numbers__column--big"></div>
+
+      <d3-pie :data-chart="racesData" />
+      <d3-pie :data-chart="sidesData" />
+    </div> -->
   </div>
 </template>
 
@@ -11,11 +18,13 @@
 import { defineComponent, computed } from '@vue/composition-api'
 
 import d3Bar from '@/d3/d3-barchart'
+import d3Pie from '@/d3/d3-piechart'
 
 export default defineComponent({
   name: 'Numbers',
   components: {
     d3Bar,
+    d3Pie,
   },
   setup(props, ctx) {
     const numbers = computed(() => {
@@ -51,13 +60,11 @@ export default defineComponent({
         id: 'lines',
         data: [
           {
-            name: `${linesInMovies.value} lines on average`,
+            name: `average in star wars movies`,
             value: linesInMovies,
           },
           {
-            name: `${
-              linesInMovie.value
-            } lines in  ${ctx.root.$store.state.movies[
+            name: `${ctx.root.$store.state.movies[
               ctx.root.$store.state.activeMovie
             ].number.toLowerCase()}`,
             value: linesInMovie,
@@ -65,12 +72,14 @@ export default defineComponent({
         ],
         size: {
           width: window.innerWidth * 0.7 * 0.3,
-          height: window.innerHeight * 0.15,
+          height: window.innerHeight * 0.25,
         },
         other: {
-          title: 'what about lines ?',
-          sub: 'lines in the movie',
-          class: 'numbers__bar--right',
+          title: 'Lines in the movie ',
+          sub: `lines in the  ${ctx.root.$store.state.movies[
+            ctx.root.$store.state.activeMovie
+          ].number.toLowerCase()}`,
+          class: 'numbers__column--small',
           value: linesInMovie.value,
         },
       }
@@ -81,13 +90,11 @@ export default defineComponent({
         id: 'words',
         data: [
           {
-            name: `${wordsInMovies.value} words on average`,
+            name: `average in star wars movies`,
             value: wordsInMovies,
           },
           {
-            name: `${
-              wordsInMovie.value
-            } words in ${ctx.root.$store.state.movies[
+            name: `${ctx.root.$store.state.movies[
               ctx.root.$store.state.activeMovie
             ].number.toLowerCase()}`,
             value: wordsInMovie,
@@ -95,25 +102,112 @@ export default defineComponent({
         ],
         size: {
           width: window.innerWidth * 0.7 * 0.4,
-          height: window.innerHeight * 0.15,
+          height: window.innerHeight * 0.25,
         },
         other: {
-          title: 'how much word in the movie ?',
-          sub: 'words in the movie',
-          class: 'numbers__bar--left',
+          title: 'how much words in the movie ?',
+          sub: `words in the ${ctx.root.$store.state.movies[
+            ctx.root.$store.state.activeMovie
+          ].number.toLowerCase()}`,
+          class: 'numbers__column--big',
           value: wordsInMovie.value,
         },
       }
     })
 
-    console.log(barChartData.value, numbers.value)
+    const racesNumbers = {
+      human: 0,
+      alien: 0,
+      droid: 0,
+    }
+
+    const sidesNumbers = {
+      dark: 0,
+      light: 0,
+      neutral: 0,
+    }
+
+    numbers.value.forEach(element => {
+      if (element.race === 'Human') racesNumbers.human++
+      if (element.race === 'Alien') racesNumbers.alien++
+      if (element.race === 'Droid') racesNumbers.droid++
+      if (element.side === 'Dark') sidesNumbers.dark++
+      if (element.side === 'Light') sidesNumbers.light++
+      if (element.side === 'Neutral') sidesNumbers.neutral++
+    })
+
+    const racesData = computed(() => {
+      return {
+        id: 'pieRace',
+        data: [
+          {
+            name: 'humans',
+            value: racesNumbers.human,
+            color: '#001eff',
+          },
+          {
+            name: 'aliens',
+            value: racesNumbers.alien,
+            color: 'red',
+          },
+          {
+            name: 'droids',
+            value: racesNumbers.droid,
+            color: '#ffe700',
+          },
+        ],
+        size: window.innerHeight * 0.25,
+        title: 'Races in the movie',
+        sub:
+          ctx.root.$store.state.activeMovie === 0
+            ? 'characters are aliens'
+            : 'characters are humans',
+        class: 'numbers__column--small',
+        value: Math.floor(
+          (Math.max(...Object.values(racesNumbers)) /
+            Object.values(racesNumbers).reduce((acc, i) => acc + i)) *
+            100
+        ),
+      }
+    })
+
+    const sidesData = computed(() => {
+      return {
+        id: 'pieSide',
+        data: [
+          {
+            name: 'dark',
+            value: sidesNumbers.dark,
+            color: 'red',
+          },
+          {
+            name: 'light',
+            value: sidesNumbers.light,
+            color: '#001eff',
+          },
+          {
+            name: 'neutral',
+            value: sidesNumbers.neutral,
+            color: '#ffe700',
+          },
+        ],
+        size: window.innerHeight * 0.25,
+        title: 'Ligth vs Dark side',
+        sub: 'characters from light side',
+        class: 'numbers__column--small',
+        value: Math.floor(
+          (Math.max(...Object.values(sidesNumbers)) /
+            Object.values(sidesNumbers).reduce((acc, i) => acc + i)) *
+            100
+        ),
+      }
+    })
 
     return {
-      numbers,
       barChartData,
       barChartDataWords,
-      linesInMovie,
-      wordsInMovie,
+      racesData,
+      sidesData,
     }
   },
 })
@@ -134,8 +228,8 @@ export default defineComponent({
 
   overflow: scroll;
 
-  &__bars {
-    width: 70%;
+  &__row {
+    width: 1100px;
 
     display: flex;
     flex-direction: row;
@@ -144,20 +238,23 @@ export default defineComponent({
 
     // padding: 0px 20px;
 
-    border-bottom: 1px solid #ffe403;
-    border-top: 1px solid #ffe403;
+    // border-bottom: 1px solid #ffe403;
+    // border-top: 1px solid #ffe403;
   }
 
-  &__bar {
+  &__column {
     display: flex;
     flex-direction: column;
     height: 60%;
     padding: 40px 0px;
 
-    &--left {
-      width: 55%;
-      margin-right: 5%;
+    &--big {
+      width: calc(100vw * 0.39);
+      margin-right: calc(100vw * 0.06);
       border-right: 1px solid #ffe403;
+    }
+    &--small {
+      width: calc(100vw * 0.18);
     }
   }
 
@@ -170,13 +267,14 @@ export default defineComponent({
     color: white;
     font-size: 80px;
     line-height: 1;
-    margin-bottom: 20px;
+    margin-bottom: 10px;
   }
 
   &__graph {
-    &--bar {
-      margin: 40px 0 20px;
-    }
+    margin: 30px 0 20px;
+  }
+  &__special {
+    font-family: 'roboto-black';
   }
 }
 </style>
