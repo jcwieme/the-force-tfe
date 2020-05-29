e4ab38b84581894236b2430d38368baf3c63e944
jcwieme <jc.wieme@outlook.com>
2020-05-29 12:53:43 +0200

    :open_hands: Optimization

---
+++ vue.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  configureWebpack: {
+    optimization: {
+      runtimeChunk: 'single',
+      splitChunks: {
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        minSize: 0,
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              const packageName = module.context.match(
+                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+              )[1]
+              return `npm.${packageName.replace('@', '')}`
+            },
+          },
+        },
+      },
+    },
+  },
+
+  productionSourceMap: false,
+}
